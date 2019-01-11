0a9d99dd4e8fef6172519bb29cfd1389d8aafe25
rodrigomatteo <rodrigo.matteo@gmail.com>
2019-01-11 17:21:44 -0500

    Products sample data set

--- src/db-data.ts
+++ src/db-data.ts
@@ -92,3 +92,150 @@ export const CUSTOMERS: any =
       "city": "Hayden"
     }
   ]
+
+export const PRODUCTS: any = 
+[
+  {
+    "PRODUCT_ID": 640,
+    "PRODUCT_NO": 6021003,
+    "NAME": "PROD 100010 MW020RCW W W40 ACCESSORIES W",
+    "ALIAS": "#15 3/8\" ZINC EXPANSION JOINT",
+    "SELL_MEASURE_UNIT_CODE": "PA",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 150,
+    "MEASURE_UNIT_CODE": "FT",
+    "LENGTH": 5,
+    "HEIGHT": 1
+  },
+  {
+    "PRODUCT_ID": 5213,
+    "PRODUCT_NO": 6021003,
+    "NAME": "PROD 100010 MW020RCW W W40 ACCESSORIES W",
+    "ALIAS": "#15 3/8\" ZINC EXPANSION JOINT",
+    "SELL_MEASURE_UNIT_CODE": "PA",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 150,
+    "MEASURE_UNIT_CODE": "FT",
+    "LENGTH": 5,
+    "HEIGHT": null
+  },
+  {
+    "PRODUCT_ID": 402,
+    "PRODUCT_NO": 6024,
+    "NAME": "DWT 250-20GA 2L 33MIL",
+    "ALIAS": "250T200-33",
+    "SELL_MEASURE_UNIT_CODE": "",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 741,
+    "MEASURE_UNIT_CODE": "",
+    "LENGTH": null,
+    "HEIGHT": null
+  },
+  {
+    "PRODUCT_ID": 4975,
+    "PRODUCT_NO": 6024,
+    "NAME": "DWT 250-20GA 2L 33MIL",
+    "ALIAS": "250T200-33",
+    "SELL_MEASURE_UNIT_CODE": "PC",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 741,
+    "MEASURE_UNIT_CODE": "IN",
+    "LENGTH": null,
+    "HEIGHT": 0.033
+  },
+  {
+    "PRODUCT_ID": 4559,
+    "PRODUCT_NO": 100006,
+    "NAME": "2.5\" SLOTTED TRACK 20GA GALV",
+    "ALIAS": "2.5\" SLOTTED TRACK 20GA GALV",
+    "SELL_MEASURE_UNIT_CODE": "",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 616,
+    "MEASURE_UNIT_CODE": "",
+    "LENGTH": null,
+    "HEIGHT": null
+  },
+  {
+    "PRODUCT_ID": 9132,
+    "PRODUCT_NO": 100006,
+    "NAME": "2.5\" SLOTTED TRACK 20GA GALV",
+    "ALIAS": "2.5\" SLOTTED TRACK 20GA GALV",
+    "SELL_MEASURE_UNIT_CODE": "PC",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 616,
+    "MEASURE_UNIT_CODE": "IN",
+    "LENGTH": null,
+    "HEIGHT": 1
+  },
+  {
+    "PRODUCT_ID": 12428,
+    "PRODUCT_NO": 4294328,
+    "NAME": "DWS 250 FG",
+    "ALIAS": "#1 VINYL CORNERBD 10' 2.5\"WING TEST",
+    "SELL_MEASURE_UNIT_CODE": "BDL",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 500,
+    "MEASURE_UNIT_CODE": "FT",
+    "LENGTH": 10,
+    "HEIGHT": 5
+  },
+  {
+    "PRODUCT_ID": 12429,
+    "PRODUCT_NO": 4294329,
+    "NAME": "DWS 250 FG",
+    "ALIAS": "#1 VINYL CORNERBD 10' 2.5\"WING TEST",
+    "SELL_MEASURE_UNIT_CODE": "BDL",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 5,
+    "MEASURE_UNIT_CODE": "FT",
+    "LENGTH": 10,
+    "HEIGHT": 5
+  },
+  {
+    "PRODUCT_ID": 12427,
+    "PRODUCT_NO": 6021088,
+    "NAME": "fg",
+    "ALIAS": "fg",
+    "SELL_MEASURE_UNIT_CODE": "",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 5,
+    "MEASURE_UNIT_CODE": "",
+    "LENGTH": null,
+    "HEIGHT": null
+  },
+  {
+    "PRODUCT_ID": 4563,
+    "PRODUCT_NO": 100010,
+    "NAME": "PROD 100010 MWFG0RCW W W40 ACCESSORIES W",
+    "ALIAS": "UDLT 2\"-20GA W/ 1.5L & 2.5L",
+    "SELL_MEASURE_UNIT_CODE": "",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "",
+    "WEIGHT": 672,
+    "MEASURE_UNIT_CODE": "",
+    "LENGTH": null,
+    "HEIGHT": null
+  },
+  {
+    "PRODUCT_ID": 9136,
+    "PRODUCT_NO": 100010,
+    "NAME": "PROD 100010 MWFG0RCW W W40 ACCESSORIES W",
+    "ALIAS": "UDLT 2\"-20GA W/ 1.5L & 2.5L",
+    "SELL_MEASURE_UNIT_CODE": "PC",
+    "WEIGHT_UNIT_CODE": "LB",
+    "WEIGHT_UNIT_SECOND_CODE": "MLF",
+    "WEIGHT": 672,
+    "MEASURE_UNIT_CODE": "IN",
+    "LENGTH": 10,
+    "HEIGHT": 0.02
+  }
+ ]
